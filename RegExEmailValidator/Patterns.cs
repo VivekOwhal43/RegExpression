10a9ab7302c57310ab1a30211f254b0bc37e74a6
VivekOwhal43 <vivekowhal.co@gmail.com>
2022-04-10 17:23:24 +0530

    [Vivek] Comments added

--- RegExEmailValidator/Patterns.cs
+++ RegExEmailValidator/Patterns.cs
@@ -6,7 +6,7 @@ namespace RegExEmailValidator
     public class Patterns
     {
         public static string name_validator = "^[A-Z][a-z]{2,}$";   // pattern of string to validate first name
-        public static string email_Validator ="^[0-9a-zA-Z]+[./+_-]{0,1}[0-9a-zA-Z]+[@][a-zA-Z0-9-]+[.][a-zA-Z]{2,}([.][a-zA-Z]{2,}){0,1}$";
+        public static string email_Validator ="^[0-9a-zA-Z]+[./+_-]{0,1}[0-9a-zA-Z]+[@][a-zA-Z0-9-]+[.][a-zA-Z]{2,}([.][a-zA-Z]{2,}){0,1}$"; // pattern for email validation
         public bool firstNameValidator(string firstName)    // method for validating name
         {
             return Regex.IsMatch(firstName,name_validator);     // true or false value will be returned 
@@ -17,7 +17,7 @@ namespace RegExEmailValidator
             return Regex.IsMatch(lastName,name_validator);     // true or false value will be returned 
         }
 
-        public bool emailValidator(string email)    // method for validating last name
+        public bool emailValidator(string email)    // method for validating email
         {
             return Regex.IsMatch(email,email_Validator);     // true or false value will be returned 
         }       
